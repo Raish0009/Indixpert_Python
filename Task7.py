8c1127f2836dbc90a57d1b4fc6bf66f54e8ad013
Raish mohammad <raishmohammad009@gmail.com>
2025-03-20 14:16:17 +0530

    Student details

--- Task7.py
+++ Task7.py
@@ -14,16 +14,16 @@ mydata['student_name']=input("Enter your name :")
 
 mydata['Experience']=input("Enter your experience :")
 
-skill1=(input("Enter your skill :"))
+skill1=(input("Enter your skill 1:"))
 skills.append(skill1)
 
-skill2=(input("Enter your skill :"))
+skill2=(input("Enter your skill 2:"))
 skills.append(skill2)
 
-skill3=(input("Enter your skill :"))
+skill3=(input("Enter your skill 3:"))
 skills.append(skill3)
 
-skill4=(input("Enter your skill :"))
+skill4=(input("Enter your skill 4:"))
 
 skills.append(skill4)
 
@@ -61,16 +61,16 @@ mydata['student_name']=input("Enter your name :")
 
 mydata['Experience']=input("Enter your experience :")
 
-skill1=(input("Enter your skill :"))
+skill1=(input("Enter your skill 1:"))
 skills.append(skill1)
 
-skill2=(input("Enter your skill :"))
+skill2=(input("Enter your skill 2:"))
 skills.append(skill2)
 
-skill3=(input("Enter your skill :"))
+skill3=(input("Enter your skill 3:"))
 skills.append(skill3)
 
-skill4=(input("Enter your skill :"))
+skill4=(input("Enter your skill 4:"))
 
 skills.append(skill4)
 
@@ -83,12 +83,6 @@ qualificationDict['Passing year']=input("Enter your Passing year :")
 qualificationList.append(qualificationDict)
 
 qualificationDict={}
-
-qualificationDict['qualification name']=input("Enter your Qualification name :")
-
-qualificationDict['qualification year']=input("Enter your Passing year :")
-
-qualificationList.append(qualificationDict)
 
 mydata['qualification']=qualificationList
 
